0b1fa1c73fc8f002680c2f10c46fba4ca46188c6
mannoodon <mannoodon@gmail.com>
2016-12-10 21:03:29 +0530

    new Classes Added

---
+++ app/src/main/java/nothing/firstapp/ExampleApp.java
@@ -0,0 +1,13 @@
+package nothing.firstapp;
+
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
+
+public class ExampleApp {
+
+    public void somethingToDo(){
+        Log.i("Something", "somethingToDo: Something");
+    }
+
+}
