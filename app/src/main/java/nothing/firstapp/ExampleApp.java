5b7d2081daadeff230b6c9a390c55022fa878b38
mannoodon <mannoodon@gmail.com>
2016-12-11 10:26:52 +0530

    New Changes in Log

--- app/src/main/java/nothing/firstapp/ExampleApp.java
+++ app/src/main/java/nothing/firstapp/ExampleApp.java
@@ -1,5 +1,6 @@
 package nothing.firstapp;
 
+import android.text.LoginFilter;
 import android.util.Log;
 
 import static android.content.ContentValues.TAG;
@@ -8,6 +9,6 @@ public class ExampleApp {
 
     public ExampleApp() {
         Log.i("Something", "Nothing");
+        Log.i("Nothing", "ExampleApp: Something");
     }
-
 }
